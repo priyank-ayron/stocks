641adb1741ea4748a88b0d2859bd780f577a5439
Priyank Agarwal <priyank.ayron@gmail.com>
2022-12-12 10:40:37 +0530

    Update urls.py

--- stocks/urls.py
+++ stocks/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.views.static import serve
-from django.conf.urls import url
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('quotes.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+    path('', include('quotes.urls'))
 ]
