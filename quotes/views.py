01e569e24e89f030aa3a681567df9e7932dd76eb
Priyank Agarwal <priyank.ayron@gmail.com>
2022-12-12 18:22:53 +0530

    Update views.py

--- quotes/views.py
+++ quotes/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render, redirect
 from .models import Stock
 from django.contrib import messages
 from .forms import StockForm
-import requests
-import json
+import finnhub
 
 
 def home(request):
     if request.method == 'POST':
-        ticker = request.POST['ticker']
-        api_req = requests.get("https://cloud.iexapis.com/stable/stock/{}/quote?token=pk_62f1e6b0a3874cde8a3c9873241088f7".format(ticker))
+        ticker = request.POST['ticker'].upper().strip()
+        finnhub_client = finnhub.Client(api_key = "cebcm5qad3if3fbt6do0cebcm5qad3if3fbt6dog")
         try:
-            api = json.loads(api_req.content)
+            api = finnhub_client.quote(ticker)
+            resp = finnhub_client.company_profile2(symbol = ticker)
+            api['name'] = resp['name']
         except Exception as e:
             api = "Error"
         return render(request, 'home.html', {'api': api})
@@ -27,17 +28,30 @@ def add_stock(request):
     if request.method == 'POST':
         form = StockForm(request.POST or None)
         if form.is_valid():
-            form.save()
-            messages.success(request, "Stock has been added.")
+            ticker = form.cleaned_data['ticker'].upper().strip()
+            try:
+                finnhub_client = finnhub.Client(api_key = "cebcm5qad3if3fbt6do0cebcm5qad3if3fbt6dog")
+                api = finnhub_client.company_profile2(symbol = ticker)
+                if len(api)==0:
+                    raise Exception("There is some problem with your ticker symbol. Please try again.")
+                form.save()
+                messages.success(request, "Stock has been added.")
+            except Exception as e:
+                messages.success(request, "There is some problem with your ticker symbol. Please try again.")
+                api = "Error"
             return redirect('add_stock')
+
     else:
         tickers = Stock.objects.all()
         output = []
+        finnhub_client = finnhub.Client(api_key = "cebcm5qad3if3fbt6do0cebcm5qad3if3fbt6dog")
         for ticker in tickers:
-            api_req = requests.get(
-                "https://cloud.iexapis.com/stable/stock/{}/quote?token=pk_62f1e6b0a3874cde8a3c9873241088f7".format(str(ticker)))
+            ticker = str(ticker)
             try:
-                api = json.loads(api_req.content)
+                ticker = ticker.upper().strip()
+                api = finnhub_client.quote(ticker)
+                resp = finnhub_client.company_profile2(symbol = ticker)
+                api['name'] = resp['name']
                 output.append(api)
             except Exception as e:
                 api = "Error"
